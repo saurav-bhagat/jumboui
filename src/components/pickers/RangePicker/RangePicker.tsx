40b2cdc0564e22d9aa892fbd3a1d30d4726ff1b3
Saurav Bhagat <saurav.bhagat@jumbotail.com>
2023-05-23 16:15:32 +0530

    added more props

--- src/components/pickers/RangePicker/RangePicker.tsx
+++ src/components/pickers/RangePicker/RangePicker.tsx
@@ -1,17 +1,24 @@
 import { DatePicker as AntDatePicker } from 'antd';
+import React from 'react';
 
 const { RangePicker: AntRangePicker } = AntDatePicker;
 
-type pickerType = 'week' | 'month' | 'year' | 'quarter';
+type pickerType = 'week' | 'month' | 'year' | 'quarter' | 'date';
 
 type RangePickerProps = {
-	// range of picker
-	picker?: pickerType
-}
 
-const RangePicker = ({ picker = 'year' }: RangePickerProps) => {
+	picker?: pickerType; /* range of picker */
+	className?: string; /* classname of picker */
+
+} & typeof AntRangePicker extends React.ComponentType<infer P> ? P : never;
+
+
+/**
+ * A Date Range Picker component
+ */
+const RangePicker = ({ picker = 'date', ...rest }: RangePickerProps) => {
 	return (
-		<AntRangePicker picker={picker} />
+		<AntRangePicker picker={picker} {...rest} />
 	);
 };
 
--- src/stories/pickers/RangePicker.stories.ts
+++ src/stories/pickers/RangePicker.stories.ts
@@ -5,6 +5,12 @@ const meta = {
 	title: 'Pickers/DatePicker',
 	component: RangePicker,
 	tags: ['autodocs'],
+	parameters: {
+		docs: {
+			//story: { inline: true }, // render the story in an iframe
+			//canvas: { sourceState: 'shown' }, // start with the source open
+		},
+	},
 	argTypes: {
 		picker: {
 			control: {
@@ -25,4 +31,12 @@ export const SimpleDateRangePicker: Story = {
 
 export const MonthRangePicker: Story = {
 	args: { picker: 'month' },
+};
+
+export const DisabledRangePicker: Story = {
+	args: { disabled: true },
+};
+
+export const DateAndTimeRangePicker: Story = {
+	args: { showTime: true },
 };
